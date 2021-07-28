2e9d379d1c2358937d36e5049118bae4293f91a1
bokyungseo <maybutter756@gmail.com>
2021-07-28 14:47:25 +0900

    modify iou.py

--- iou.py
+++ iou.py
@@ -1,33 +1,63 @@
 import math
 
-def intersection_over_union(box1, box2):  # list, list
+def get_area(box):
+    return (box[2] - box[0] + 1) * (box[3] - box[1] + 1)
+
+
+def get_union_areas(box1, box2, interArea=None):
+    area_A = get_area(box1)
+    area_B = get_area(box2)
+
+    if interArea is None:
+        interArea = get_intersection_area(box1, box2)
+
+    return float(area_A + area_B - interArea)
+
+
+def get_intersection_area(box1, box2):
     x1 = max(box1[0], box2[0])
     y1 = max(box1[1], box2[1])
     x2 = min(box1[2], box2[2])
     y2 = min(box1[3], box2[3])
 
-    area_intersection = (x2 - x1) * (y2 - y1)
-    area_box1 = (box1[2] - box1[0]) * (box1[3] - box1[1])
-    area_box2 = (box2[2] - box2[0]) * (box2[3] - box2[1])
-    area_union = area_box1 + area_box2 - area_intersection
+    return (x2 - x1 + 1) * (y2 - y1 + 1)
 
-    iou = area_intersection / area_union
-    return iou
+
+def boxes_intersect(box1, box2):
+    if box1[0] > box2[2]:
+        return False
+    if box2[0] > box1[2]:
+        return False
+    if box1[3] < box2[1]:
+        return False
+    if box1[1] > box2[3]:
+        return False
+    return True
+
+
+def intersection_over_union(box1, box2):  # list, list
+    if boxes_intersect(box1, box2) is False:
+        return 0
+    interArea = get_intersection_area(box1, box2)
+    union = get_union_areas(box1, box2, interArea=interArea)
+
+    result = interArea / union
+    assert result >= 0
+    return result
 
 
 def indexing_removal_with_iou(output):  # save and return indexes to dismiss
     will_remove_indexes = set()
     if len(output["boxes"]) <= 1:
         return will_remove_indexes
-    for i in range(len(output["boxes"]) - 1):
-        for j in range(1, len(output["boxes"])):
+    for i in range(0, len(output["boxes"]) - 1):
+        for j in range(i+1, len(output["boxes"])):
             box1 = output["boxes"][i]
             box2 = output["boxes"][j]
             score1, score2 = output["scores"][i], output["scores"][j]
-            if 0.7 < abs(intersection_over_union(box1, box2)) < 1:  # need to optimize range 0.7 ~ 0.85
-                if score1 > score2:
-                    will_remove_indexes.add(j)
-                else:
-                    will_remove_indexes.add(i)
+            if 0.45 < intersection_over_union(box1, box2) < 1 and score1 > score2:
+                will_remove_indexes.add(j)
+            elif 0.45 < intersection_over_union(box1, box2) < 1 and score1 < score2:
+                will_remove_indexes.add(i)
 
     return will_remove_indexes
