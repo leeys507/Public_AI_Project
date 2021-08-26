f5458eaca5a1223703ac6845c1045ed4ab741d9d
sungyub <lumitrail@gmail.com>
2021-08-26 13:25:17 +0900

    word2vec study

---
+++ word2vec/initial.py
@@ -0,0 +1,11 @@
+# conda install gensim
+# nltk
+# lxml
+# pip intall konlpy
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import urllib.request
+from gensim.models.word2vec import Word2Vec
+from konlpy.tag import Okt
+
