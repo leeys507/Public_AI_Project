1600147cc49f5a4e86f573d4046ad1a8f6a4c9cc
sungyub <lumitrail@gmail.com>
2021-08-27 14:37:01 +0900

    manual labeling code

--- classification/FingerSnap.py
+++ classification/FingerSnap.py
@@ -1,5 +1,7 @@
 import os
 import glob
+import random
+from fingerSnapUtil import *
 
 # path.txt 파일 안에 60만 txt파일들이 있는 디렉토리를 써놔요
 # ex) path.txt << C:/Users/me/Desktop/txtfiles
@@ -7,16 +9,18 @@ pathFile = open("./path.txt", "r", encoding="utf8")
 path = pathFile.readline()
 pathFile.close()
 
-count = "000"
-# count = 000 ~ 622
-
-txtFiles = glob.glob(os.path.join(path, "KsponSpeech_"+count+"*.txt"))
+txtFiles = []
+while len(txtFiles) < 1:
+    count = str(random.randrange(0, 623))
+    # count = 000 ~ 622
+    txtFiles = glob.glob(os.path.join(path, "KsponSpeech_"+count+"*.txt"))
 
 dataFile = open("./data.csv", "a", encoding="utf8")
 
 for txtFile in txtFiles:
-    txtFile = open(txtFile, "r", encoding="euc-kr")
-    for line in txtFile.readlines():
+    fp = open(txtFile, "r", encoding="euc-kr")
+    for line in fp.readlines():
+        line = strip(line)
         validLabels = ["0", "1", "2", "3", "4", "n"]
         label = "999"
         while not(label in validLabels):
@@ -26,12 +30,11 @@ for txtFile in txtFiles:
                 exit()
         if label == "n":
             continue
-        lineOut = "\"" + line[:-1] + "\"," + label + "\n"
+        lineOut = "\"" + line + "\"," + label + "\n"
         lineOut = lineOut.encode('utf-8')
         lineOut = lineOut.decode('utf-8')
-        print(lineOut)
-        exit()
         dataFile.write(lineOut)
-    txtFile.close()
+    fp.close()
+    os.remove(txtFile)
 
 dataFile.close()
---
+++ classification/fingerSnapUtil.py
@@ -0,0 +1,13 @@
+# b/ : 숨소리
+# n/ : 주변잡음
+# l/ : 웃음
+# o/ : 다른 사람과 겹침
+# aaa(aaa) : 이중 전사
+# 응/ : 간투어
+# +나 *는 지워도 됨
+
+def strip(line):
+    badChars = ["b/", "n/", "l/", "o/", "+", "*","\n"]
+    for c in badChars:
+        line = line.replace(c, "")
+    return line
