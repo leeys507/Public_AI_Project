5f9a1a14c8166854c55d89d19083a6e61413412a
bokyungseo <maybutter756@gmail.com>
2021-09-02 14:12:08 +0900

    modify noise-reduction prop

--- stt.py
+++ stt.py
@@ -8,10 +8,6 @@ import glob
 subscription_key = '539b71356bc54e8a9655d3c59e47e625'
 endpoint = 'https://koreacentral.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1'
 
-# filename
-test_filename = 'data/wave_data/KsponSpeech_000011.wav'
-save_path = 'data/label/'
-
 headers = {
     'Ocp-Apim-Subscription-Key': subscription_key,
     # 'Content-type': 'audio/wav; codecs=audio/pcm; samplerate=16000',
@@ -48,7 +44,7 @@ def folder_to_filepaths(folder_path):
 def noise_reduction(file_path):
     rate, data = wavfile.read(file_path)
     # noise_reduced_data = nr.reduce_noise(y=data, sr=rate, n_std_thresh_stationary=1, stationary=True)
-    noise_reduced_data = nr.reduce_noise(y=data, sr=rate, thresh_n_mult_nonstationary=1.1)
+    noise_reduced_data = nr.reduce_noise(y=data, sr=rate, prop_decrease=0.6)
     wavfile.write('tmp.wav', data=noise_reduced_data, rate=rate)
     with open('tmp.wav', 'rb') as f:
         stream_file = f.read()
